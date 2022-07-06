8f463790f07514208bdd3b143354d7ba652814c0
danielereich1 <86350020+danielereich1@users.noreply.github.com>
2022-07-06 23:08:57 +0200

    Add files via upload

---
+++ 01a_Basics/01_Intro/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <MyComponent />
+  </div>
+</template>
+
+<script>
+/* Import all components */
+import Demos from './components/VueDirectives'
+
+export default {
+  components: {
+    MyComponent: Demos
+  },
+}
+</script>
+
+<style lang="scss">
+//Import of global-style
+@import 'styles/global';
+</style>
---
+++ 01a_Basics/01_Intro/src/components/VueDirectives.vue
@@ -0,0 +1,51 @@
+<template>
+  
+  <div>
+    <h1 v-if="false" v-text="text">Loading...</h1>
+    <h2 v-else-if="false" v-html="html" />
+    <a
+      v-else
+      :href="link.url"
+      :target="link.target"
+      :tabindex="link.tabindex"
+      v-text="link.title"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // v-text
+      text: 'Have a look at the NBA Website',
+      // v-html
+      html: 'The link </br>is shortened<br/><b>but its the right one</b>',
+      // v-bind
+      link: {
+        url: 'https://www.nba.com/',
+        target: '_blank',
+        tabindex: '0',
+        title: 'NBA Website here',
+      },
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  color: #fdb927;
+}
+
+h2 {
+  margin: 4rem 0 0;
+  font-weight: normal;
+  color: #006bb6;
+}
+
+a {
+  display: block;
+  margin-top: 4rem;
+}
+</style>
